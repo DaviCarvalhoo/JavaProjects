c07f0fd704cfa5ca99aee0d6f310f68bb8b09bb8
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-06-01 20:28:29 -0300

    variaveis em java

---
+++ Basic/src/Variaveis.java
@@ -0,0 +1,9 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade;
+        String meuNome;
+        meuNome = "Davi";
+        idade = 42;
+        System.out.println("Nome: " + meuNome+ "\nIdade: "+idade);
+    }
+}
